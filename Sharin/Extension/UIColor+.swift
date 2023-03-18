7fab0b21ddcb6137cb7f3abcce61a9123dc21bb4
서광현 (seo-kh) <gospelseokh@gmail.com>
2023-03-19 00:15:56 +0900

    기본 에셋 및 컬러 및 아이콘 설정하기

---
+++ Sharin/Extension/UIColor+.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+.swift
+//  Sharin
+//
+//  Created by james seo on 2023/03/19.
+//
+
+import UIKit
+
+extension UIColor {
+    static let sharinPrimary = UIColor(named: "sharinPrimary")
+    static let sharinSecondary = UIColor(named: "sharinSecondary")
+    static let sharinTertiary = UIColor(named: "sharinTertiary")
+    static let sharinQuaternary = UIColor(named: "sharinQuaternary")
+}
