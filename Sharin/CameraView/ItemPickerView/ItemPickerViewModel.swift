dbe57b032faba76ca73d09db61e5ba277deb53ce
서광현 (seo-kh) <gospelseokh@gmail.com>
2023-04-21 00:45:33 +0900

    [Chore] ItemPickerViewController - dismiss시 버그 수정 / 아이템 선택, x버튼을 눌러야만 dismiss가 해제되는 버그 수정 > 아이템 선택 또는 x버튼을 눌르면 dismiss 되도록 수정

--- Sharin/CameraView/ItemPickerView/ItemPickerViewModel.swift
+++ Sharin/CameraView/ItemPickerView/ItemPickerViewModel.swift
@@ -11,7 +11,7 @@ import UIKit
 final class ItemPickerViewModel: NSObject {
     let itemPick = CurrentValueSubject<Item?, Never>(nil)
     let ipvc = PassthroughSubject<ItemPickerViewContrller, Never>()
-    let dismiss = PassthroughSubject<Void, Never>()
+    let dismiss = CurrentValueSubject<Void, Never>(())
     
     override init() {
         ipvc
