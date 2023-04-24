5439fdd52b81966d41a4e320fac8755cf8f658da
서광현 (seo-kh) <gospelseokh@gmail.com>
2023-04-24 13:55:02 +0900

    [Chore] ItemCell 레이브글씨가 씹히는 현상 제거

--- Sharin/CameraView/ItemPickerView/ItemCell.swift
+++ Sharin/CameraView/ItemPickerView/ItemCell.swift
@@ -9,52 +9,49 @@ import UIKit
 
 final class ItemCell: UICollectionViewCell {
     static let identifier = "ItemCell"
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
+
+    private lazy var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(imageView)
-        
-        NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: self.topAnchor),
-            imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-        ])
-        
         imageView.layer.cornerRadius = 12.0
         imageView.layer.borderWidth = 1.0
         imageView.layer.borderColor = UIColor.gray.withAlphaComponent(0.2).cgColor
         imageView.layer.shadowRadius = 3.0
         imageView.layer.shadowColor = UIColor.gray.cgColor
         imageView.layer.shadowOffset = .init(width: 10.0, height: 10.0)
-        imageView.layer.masksToBounds = false
-        contentView.clipsToBounds = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .headline)
+        label.numberOfLines = 1
+        label.showsExpansionTextWhenTruncated = true
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        [imageView, nameLabel].forEach { contentView.addSubview($0) }
+
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+            nameLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -8.0),
+            nameLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 12.0)
+        ])
     }
-    
+
     func setCell(from item: Item) {
-        if let view = contentView.subviews.first as? UIImageView {
-            view.image = UIImage(named: item.usdz)
-            view.contentMode = .scaleAspectFit
-            
-            let label = UILabel()
-            label.text = item.name
-            label.font = .preferredFont(forTextStyle: .headline)
-            label.translatesAutoresizingMaskIntoConstraints = false
-            label.numberOfLines = 1
-            label.showsExpansionTextWhenTruncated = true
-            
-            view.addSubview(label)
-            
-            NSLayoutConstraint.activate([
-                label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -8.0),
-                label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12.0)
-            ])
-        }
+        imageView.image = UIImage(named: item.usdz)
+        nameLabel.text = item.name
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
