02b17a1ec62b3d7cf8755ee59296ba1e4dd940db
서광현 (seo-kh) <gospelseokh@gmail.com>
2023-04-26 16:44:57 +0900

    [Refactor] ItemStore, NetworkManager를 상위 계층으로 분리

---
+++ Sharin/ContentViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContentViewModel.swift
+//  Sharin
+//
+//  Created by james seo on 2023/04/26.
+//
+
+import Foundation
+
+final class ContentViewModel {
+    let itemStore = ItemStore()
+    let networkManager = NetworkManager()
+}
